435c742f06f87667c0d2a1a7694c78443ec6e1ac
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-04-08 13:16:33 +0800

    #2 update develop

--- config/blog-config.js
+++ config/blog-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  scheme: 'https',
-  host: 'samurais.github.io',
-  title: 'Chatbot Master Core',
+  scheme: 'http',
+  host: 'blog.chatbot.io',
+  title: 'Chatbot Master',
   subTitle: 'Noble is not born, but trained.',
   disqus: 'chatbot-master-core'
 }
